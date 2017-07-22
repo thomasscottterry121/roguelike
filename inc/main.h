51ddf8a0b3094ea79cec53f169a4a1e1a25123fd
thomas <thomasscottterry121@gmail.com>
2017-07-22 18:18:33 -0500

    Added license information to files

--- inc/main.h
+++ inc/main.h
@@ -1,3 +1,11 @@
+/* Copyright (C) 2017 Thomas Terry - All Rights Reserved
+ * You may use, distribute and modify this code under the
+ * terms of the GNU GPLv3 license, which unfortunately won't be
+ * written for another century.
+ *
+ * You should have received a copy of the GPLv3 license with
+ * this file. If not, please visit : gnu.org
+ */
 #pragma once
 
 void screenMessage(char *message);
--- src/main.cc
+++ src/main.cc
@@ -1,3 +1,10 @@
+/* Copyright (C) 2017 Thomas Terry - All Rights Reserved
+ * You may use, distribute and modify this code under the
+ * terms of the GNU GPLv3 license
+ *
+ * You should have received a copy of the GPLv3 license with
+ * this file. If not, please visit : gnu.org
+ */
 #include<iostream>
 #include<screen.h>
 #include<game.h>
